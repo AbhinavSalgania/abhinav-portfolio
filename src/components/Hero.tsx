e726f1f49255f80cca9c9345d588d069f868ac82
Abhinav Salgania <abhinavsalgania@gmail.com>
2023-02-07 09:17:52 -0330

    add Hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {Cursor, useTypewriter} from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
+import Link from 'next/link'
+
+type Props = {}
+
+export default function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            'Hi, My name is Abhinav Salgania', 
+            'I am a 3rd year undergraduate student', 
+            'I love coffee',
+            'I love gym',
+            'I love coding',
+        ],
+        loop: true,
+        delaySpeed: 2000,
+
+    })
+    
+  return (
+  <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+    <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src='https://github.com/abhinavsalgania.png'
+        alt=""
+      />
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]' >Software Developer</h2>
+        <h1 className='text-2xl lg:text-4xl font-semibold px-10'>
+            <span> {text} </span>
+            <Cursor cursorColor='#F7AB0A' /> 
+        </h1>
+
+        <div className='pt-5'>
+            <Link href='#about'>
+            <button className='hereButton'>About</button>
+            </Link>
+            <Link href='#experience'>
+            <button className='hereButton'>Experience</button>
+            </Link>
+            <Link href='#skills'>
+            <button className='hereButton'>Skills</button>
+            </Link>
+            <Link href='#projects'>
+            <button className='hereButton'>Projects</button>
+            </Link>
+        </div>
+
+      </div>
+  </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,23 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Abhinav Salgania Portfolio</title>
       </Head>
 
       <Header />
-      
-      {/*Hero*/}
 
+      {/*Hero*/}
+      <section id="hero" className='snap-center'>
+        <Hero />
+      </section>
       {/*About*/}
 
       {/*Experience*/}
@@ -25,6 +28,6 @@ export default function Home() {
 
       {/*Contact*/}
 
-    </>
+    </div>
   )
 }
