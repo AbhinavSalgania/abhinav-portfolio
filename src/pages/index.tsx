22c085d7eb88277a36cf674619aca23379b05cf0
Abhinav Salgania <abhinavsalgania@gmail.com>
2023-02-08 01:13:24 -0330

    add Skills section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Inter } from '@next/font/google'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
-
+import Skills from '../components/Skills'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,7 +27,9 @@ export default function Home() {
       </section>
   
       {/*Skills*/}
-      
+      <section id="skills" className='snap-start'>
+        <Skills />
+      </section>
 
       {/*Projects*/}
       {/*Contact*/}
