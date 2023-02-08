5612d8a68ebe1965fb265411e0d0ec5dd5b8afb5
Abhinav Salgania <abhinavsalgania@gmail.com>
2023-02-08 07:20:55 -0330

    remove apos

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -31,7 +31,7 @@ function ContactMe({}: Props) {
         <div className="flex flex-col space-y-4 md:space-y-5 lg:space-y-6 xl:space-y-6 2xl:space-y-10">
             <h4 className="text-white/90  text-xl md:text-2xl font-semibold text-center">
                 I have got just what you need.{` `}
-                <span className='decoration-[cyan]/50 underline'> Let&apos;s talk.</span>
+                <span className='decoration-[cyan]/50 underline'> Lets talk.</span>
             </h4>
             
             <div className="flex flex-col items-center space-y-1 md:space-y-3 lg:space-y-3 xl:space-y-3 2xl:space-y-5">
