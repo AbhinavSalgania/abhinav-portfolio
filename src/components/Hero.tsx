f9b5b25edef5efa968185f24f1064e52bde67352
Abhinav Salgania <abhinavsalgania@gmail.com>
2023-02-08 09:05:20 -0330

    add Background Circles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,11 +17,11 @@ export default function Hero({}: Props) {
         ],
         loop: true,
         delaySpeed: 2000,
-
     })
     
   return (
   <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+    <BackgroundCircles />
     <img
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src = {abhinav.src}
