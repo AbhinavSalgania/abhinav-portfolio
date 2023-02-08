b267f177f15ce8bab096cb12bd4b0154e31d3bae
Abhinav Salgania <abhinavsalgania@gmail.com>
2023-02-08 04:04:54 -0330

    add color to scrollbar using tailwind-scrollbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[cyan]/80'>
       <Head>
         <title>Abhinav Salgania Portfolio</title>
       </Head>
